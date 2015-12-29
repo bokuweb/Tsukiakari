64fbc1e5ac705f769aa047a51c9065110a18237e
bokuweb <info@typebeats.com>
2015-12-29 09:10:55 +0900

    Add React & redux skeleton

---
+++ src/renderer/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import configureStore from './stores/configure-store';
+import App from './containers/app';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('tsukiakari')
+);
---
+++ src/renderer/src/stores/configure-store.js
@@ -0,0 +1,13 @@
+import {createStore,  applyMiddleware} from 'redux';
+import reducers from '../reducers'
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+export default function configureStore() {
+  const logger = createLogger();
+  const createStoreWithMiddleware = applyMiddleware(
+    thunk,
+    logger
+  )(createStore);
+  return createStoreWithMiddleware(reducers);
+}
