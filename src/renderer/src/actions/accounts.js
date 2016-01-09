38aade6ebf374bfefddd70cd6ff7c74e932c3734
bokuweb <info@typebeats.com>
2016-01-10 00:12:34 +0900

    refactor

--- src/renderer/src/actions/accounts.js
+++ src/renderer/src/actions/accounts.js
@@ -6,7 +6,8 @@ export const loadAccounts = () => {
   return dispatch => {
     const path = remote.getGlobal('accountFilePath');
     jsonfile.readFile(path, (err, accounts) => {
-      dispatch(createAction('LOAD_ACCOUNTS')({accounts}));
+      const action = createAction('LOAD_ACCOUNTS');
+      dispatch(action({accounts}));
     });
   };
 };
