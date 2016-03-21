985ece85da5cf0edc40605ca2c74fc0ca6545e9b
bokuweb <bokuweb12@gmail.com>
2016-03-21 21:22:56 +0900

    Refactor main process

--- src/browser/src/main.js
+++ src/browser/src/main.js
@@ -1,34 +1,22 @@
 'use strict';
 
-// import electron from 'electron';
-const electron = require('electron');
+const { BrowserWindow, app } = require('electron');
 const client = require('electron-connect').client;
-const app = electron.app;  // Module to control application life.
 const Auth = require('./auth');
 const jsonfile = require('jsonfile');
 const _ = require('lodash');
 const ipc = require('ipc');
 const util = require('util');
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
-// Report crashes to our server.
-//electron.crashReporter.start();
-
-// Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  if (process.platform != 'darwin') app.quit();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
   let accounts = [];
   global.consumerKey = "njiDlWzzRl1ReomcCmvhbarN7";
@@ -36,17 +24,14 @@ app.on('ready', function() {
   global.accountFilePath = `${app.getPath('cache')}/accounts.json`;
 
   const loadMainWindow = () => {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
-      'node-integration': true
+      'min-width': '600px',
+      'min-height': '400px',
     });
-    // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/../../renderer/index.html`);
     client.create(mainWindow);
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
-    // Emitted when the window is closed.
-
   };
 
   try {
@@ -68,15 +53,17 @@ app.on('ready', function() {
   const addAccountUnlessExist = (account) => {
     account._id = accounts.length;
     if (!_.includes(_.map(accounts, 'id'), account.id)) accounts.push(account);
-    jsonfile.writeFile(accountFilePath, accounts,  err => console.dir(err));
+    jsonfile.writeFile(accountFilePath, accounts,  err => console.log(err));
   };
 
-  ipc.on('authenticate-request', (event, arg) => {
+  ipc.on('authenticate-request', event => {
     authenticate().then(account => {
       addAccountUnlessExist(account);
       event.sender.send('authenticate-request-reply', accounts);
     });
   });
+
+  ipc.on('accounts-request', event => event.sender.send('accounts-request-reply', accounts));
 
   if (accounts[0] !== undefined) {
     loadMainWindow();
