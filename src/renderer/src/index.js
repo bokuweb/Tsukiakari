564a44a74f4e233fdbaf10427d3ebb9d5b323732
bokuweb <bokuweb12@gmail.com>
2016-03-24 22:05:42 +0900

    Update develop env

--- src/renderer/src/index.js
+++ src/renderer/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './app/stores/configure-store';
-import App from './app/containers/app';
+import configureStore from './stores/configure-store';
+import App from './containers/app';
 
 const store = configureStore();
 
