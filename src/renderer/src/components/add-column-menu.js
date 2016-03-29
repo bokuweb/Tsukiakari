73eb3502f7d65431cb662ea8d2bb85dcbf4d0ea3
bokuweb <bokuweb12@gmail.com>
2016-03-29 21:59:46 +0900

    add

--- src/renderer/src/components/add-column-menu.js
+++ src/renderer/src/components/add-column-menu.js
@@ -1,7 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { Button } from 'react-bulma';
-import ColumnTitle from './column-title';
-import AccountSelector from './account-selector';
 import ItemSelector from './item-selector';
 
 const defaultItems = [
@@ -42,37 +40,8 @@ export default class AddColumnMenu extends Component {
     accounts: [],
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      accountId: '',
-      title: '',
-      items: {},
-    };
-    this.onTitleChange = ::this.onTitleChange;
-    this.onAccountSelect = ::this.onAccountSelect;
-    this.onItemChange = ::this.onItemChange;
-  }
-
-  onTitleChange(title) {
-    console.log(title)
-    this.setState({ title });
-  }
-
-  onAccountSelect(accountId) {
-    console.dir(accountId);
-    this.setState({ accountId, items: { [accountId]: defaultItems } });
-  }
-
-  onItemChange(value) {
-    const items = this.state.items[this.state.accountId].map(item => {
-      if (item.value === value) {
-        return Object.assign({}, item, { checked: !item.checked });
-      }
-      return item;
-    });
-    this.setState({ items: Object.assign({}, { [this.state.accountId]: items }) }, () => console.dir(this.state));
-    console.dir(items);
+  onItemClick(value) {
+    console.log(value);
   }
 
   render() {
@@ -85,13 +54,15 @@ export default class AddColumnMenu extends Component {
           <span className="add-column-menu__title">Add new column</span>
           <i className="add-column-menu__icon--close lnr lnr-cross" onClick={this.props.close} />
         </div>
-        <ColumnTitle onChange={this.onTitleChange} />
-        <AccountSelector accounts={this.props.accounts} onSelect={this.onAccountSelect} />
-        <ItemSelector onChange={this.onItemChange} items={this.state.items[this.state.accountId]} />
-        <div className="add-column-menu__bottons">
+        <ItemSelector
+          onClick={this.onItemClick}
+          items={defaultItems}
+          style={{ marginTop: '10px' }}
+        />
+        {/*<div className="add-column-menu__bottons">
           <Button>Add</Button>
           <Button>Cancel</Button>
-        </div>
+        </div> */}
       </div>
     );
   }
--- src/renderer/src/components/item-selector.js
+++ src/renderer/src/components/item-selector.js
@@ -1,27 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 
-export default class AccountSelector extends Component {
+export default class ItemSelector extends Component {
   static propTypes = {
-    onChange: PropTypes.func,
+    onClick: PropTypes.func,
+    items: PropTypes.array,
   };
 
   static defaultProps = {
-
+    items: [],
   }
 
   renderItems() {
-    if (!this.props.items) return null;
     return this.props.items.map(item => {
-      const onChange = this.props.onChange.bind(this, item.value);
+      const onClick = this.props.onClick.bind(this, item.value);
       return (
-        <li className="item-selector__li" key={item.value}>
-          <input
-            type="checkbox"
-            checked={item.checked}
-            name={item.value}
-            value={item.value}
-            onChange={onChange}
-          />
+        <li className="item-selector__li" key={item.value} onClick={onClick}>
           <i className={`item-selector__icon--list ${item.icon}`} />
           <span className="item-selector__text">{item.value}</span>
         </li>
@@ -31,11 +24,10 @@ export default class AccountSelector extends Component {
 
   render() {
     return (
-      <div className="item-selector" >
+      <div className="item-selector" style={this.props.style}>
         <div className="item-selector__title-wrapper" >
           <i className="item-selector__icon--users fa fa-check-square-o" />
-          <span className="item-selector__title">Item</span>
-          <span className="item-selector__name">@selected_account</span>
+          <span className="item-selector__title">Choose column type</span>
         </div>
         <div className="item-selector__items" >
           <ul className="item-selector__ul">
