0c73b3c6497fa9764d3490acb1fb5b14c02b1a43
bokuweb <bokuweb12@gmail.com>
2016-04-24 22:05:35 +0900

    close #26

--- src/renderer/src/components/tweetitem-footer.js
+++ src/renderer/src/components/tweetitem-footer.js
@@ -1,30 +1,50 @@
 import React, { Component, PropTypes } from 'react';
+import { B as b_ } from 'b_';
+
+const b = b_({
+  tailSpace: ' ',
+  elementSeparator: '__',
+  modSeparator: '--',
+  modValueSeparator: '-',
+  classSeparator: ' ',
+}).with('tweetitem-footer');
 
 export default class TweetItemFooter extends Component {
   static propTypes = {
     tweet: PropTypes.object,
     createFavorite: PropTypes.object,
+    accounts: PropTypes.array,
   };
+
+  constructor(props) {
+    super(props);
+    this.onFavorite = ::this.onFavorite;
+  }
 
   shouldComponentUpdate(nextProps) {
     return this.props.tweet.id !== nextProps.tweet.id;
   }
 
+  onFavorite() {
+    const { tweet } = this.props;
+    this.props.createFavorite(this.props.accounts[0], tweet.id_str);
+  }
+
   render() {
     const { tweet } = this.props;
     return (
-      <div className="tweetitem-footer">
-        <span className="tweetitem-footer__icon-wrapper">
-          <i className="tweetitem-footer__icon fa fa-reply" />
+      <div className={b()}>
+        <span className={b('icon-wrapper')}>
+          <i className={`${b('icon')} fa fa-reply`} />
         </span>
-        <span className="tweetitem-footer__icon-wrapper">
-          <i className="tweetitem-footer__icon fa fa-retweet" />
+        <span className={b('icon-wrapper')}>
+          <i className={`${b('icon')} fa fa-retweet`} />
           {tweet.retweet_count || ''}
         </span>
-        <span className="tweetitem-footer__icon-wrapper">
+        <span className={b('icon-wrapper')}>
           <i
-            className="tweetitem-footer__icon tweetitem-footer__icon--favorite fa fa-heart"
-            onClick={() => this.props.createFavorite(this.props.accounts[0], tweet.id_str)}
+            className={`${b('icon', { favorite: !tweet.favorited || 'active' })} fa fa-heart`}
+            onClick={this.onFavorite}
           />
           {tweet.favorite_count || ''}
         </span>
