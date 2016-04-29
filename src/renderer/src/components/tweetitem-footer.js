f39e93133a30c06a3a0a875fb0c5ef7eaaded599
bokuweb <bokuweb12@gmail.com>
2016-04-29 15:34:52 +0900

    Add color to footer icon

--- src/renderer/src/components/tweetitem-footer.js
+++ src/renderer/src/components/tweetitem-footer.js
@@ -18,11 +18,23 @@ export default class TweetItemFooter extends Component {
 
   constructor(props) {
     super(props);
+    this.onReply = ::this.onReply;
+    this.onRetweet = ::this.onRetweet;
     this.onFavorite = ::this.onFavorite;
   }
 
   shouldComponentUpdate(nextProps) {
     return this.props.tweet.id !== nextProps.tweet.id;
+  }
+
+  onReply() {
+
+  }
+
+  onRetweet() {
+    const { tweet } = this.props;
+    console.log('retweet')
+    // this.props.createFavorite(this.props.accounts[0], tweet.id_str);
   }
 
   onFavorite() {
@@ -35,10 +47,16 @@ export default class TweetItemFooter extends Component {
     return (
       <div className={b()}>
         <span className={b('icon-wrapper')}>
-          <i className={`${b('icon')} fa fa-reply`} />
+          <i
+            className={`${b('icon', { reply: true })} fa fa-reply`}
+            onClick={this.onReply}
+          />
         </span>
         <span className={b('icon-wrapper')}>
-          <i className={`${b('icon')} fa fa-retweet`} />
+          <i
+            className={`${b('icon', { retweet: !tweet.retweeted || 'active' })} fa fa-retweet`}
+            onClick={this.onRetweet}
+          />
           {tweet.retweet_count || ''}
         </span>
         <span className={b('icon-wrapper')}>
