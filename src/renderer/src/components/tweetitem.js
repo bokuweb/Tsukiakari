2945b0fc50cc0c0d5e50c4031296228d8e445f90
bokuweb <bokuweb12@gmail.com>
2016-05-22 15:00:48 +0900

    Add overlay

--- src/renderer/src/components/tweetitem.js
+++ src/renderer/src/components/tweetitem.js
@@ -4,7 +4,7 @@ import { decodeHtml } from '../utils/utils';
 import TweetItemFooter from './tweetitem-footer';
 import { link } from 'autolinker';
 import { htmlEscape } from 'twitter-text';
-import { default as Video, Controls, Play, Mute, Seek, Time } from 'react-html5video';
+import { default as Video, Controls, Play, Mute, Seek, Time, Overlay } from 'react-html5video';
 
 const b = B.with('tweetitem');
 
@@ -119,6 +119,7 @@ export default class TweetItem extends Component {
       <div className={b('media')}>
         <Video className={b('video')} controls>
           { video.variants.map(variant => <source src={variant.url} />) }
+        <Overlay />
         <Controls>
           <Play />
           <Seek />
