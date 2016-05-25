9052d388d0a13789809d8922b49fb75d35ee616d
bokuweb <bokuweb12@gmail.com>
2016-05-25 13:25:58 +0900

    Implement account remove

--- src/browser/src/main.js
+++ src/browser/src/main.js
@@ -43,16 +43,34 @@ app.on('ready', () => {
       .catch(error => console.log(error));
   });
 
-  const addAccountUnlessExist = (account) => {
+  const addAccountUnlessExist = account => new Promise((resolve, reject) => {
     if (!_.includes(_.map(accounts, 'id_str'), account.id_str)) accounts.push(account);
-    jsonfile.writeFile(accountFilePath, accounts, err => console.log(err));
+    jsonfile.writeFile(accountFilePath, accounts, (err, obj) => {
+      if (err) reject(err);
+      resolve(obj);
+    });
+  });
+
+  const signIn = () => new Promise((resolve, reject) => {
+    authenticate()
+      .then(addAccountUnlessExist)
+      .then(resolve)
+      .catch(reject);
+  });
+
+  const removeAccount = id => {
+    // TODO: Refactor
+    try {
+      accounts = jsonfile.readFileSync(accountFilePath);
+    } catch (e) {
+      console.log(e);
+    }
+    accounts = accounts.filter(account => account.id_str !== id);
+    jsonfile.writeFileSync(accountFilePath, accounts);
   };
 
   ipcMain.on('authenticate-request', event => {
-    authenticate().then(account => {
-      addAccountUnlessExist(account);
-      event.sender.send('authenticate-request-reply', accounts);
-    });
+    signIn().then(() => event.sender.send('authenticate-request-reply', accounts));
   });
 
   ipcMain.on('accounts-request', event => {
@@ -60,18 +78,16 @@ app.on('ready', () => {
   });
 
   ipcMain.on('remove-account-request', (event, account) => {
-    console.log(account);
-    // event.sender.send('accounts-request-reply', accounts);
+    removeAccount(account.id_str);
+    if (accounts.length === 0) {
+      mainWindow.destroy();
+      return signIn().then(() => loadMainWindow());
+    }
+    event.sender.send('remove-account-request-reply', accounts);
   });
 
-  if (accounts[0] !== undefined) {
-    loadMainWindow();
-  } else {
-    authenticate().then(account => {
-      addAccountUnlessExist(account);
-      loadMainWindow();
-    });
-  }
+  if (accounts[0] !== undefined) loadMainWindow();
+  else signIn().then(() => loadMainWindow());
 
   powerMonitor.on('suspend', () => {
     console.log("suspend!!!");
