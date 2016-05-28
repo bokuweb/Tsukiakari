abc6e7d86b0a41050fd8bbfc8879482e8a8e0f3d
bokuweb <bokuweb12@gmail.com>
2016-05-28 12:56:45 +0900

    Add spinner

---
+++ src/renderer/src/components/spinner.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Spinner extends Component {
+  static propTypes = {
+    style: PropTypes.object,
+  };
+
+  static defaultProps = {
+    style: {},
+  };
+
+  render() {
+    return (
+      <div className="sk-folding-cube" style={this.props.style} >
+        <div className="sk-cube1 sk-cube"></div>
+        <div className="sk-cube2 sk-cube"></div>
+        <div className="sk-cube4 sk-cube"></div>
+        <div className="sk-cube3 sk-cube"></div>
+      </div>
+    );
+  }
+}
