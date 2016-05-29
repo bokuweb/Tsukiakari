f6dbc9ceeb0acc1a90eb9feb739eb96cc44e68f3
bokuweb <bokuweb12@gmail.com>
2016-05-29 22:37:45 +0900

    Fix bug, not set favorite and retweet immediately
    - set connect option { pure: false }

--- src/renderer/src/components/tsukiakari.js
+++ src/renderer/src/components/tsukiakari.js
@@ -22,7 +22,7 @@ export default class Tsukiakari extends Component {
   }
 
   render() {
-    if (this.props.accountLength === 0) return <Spinner style={{ padding: '20p% 0 0 0' }} />;
+    if (this.props.accountLength === 0) return <Spinner style={{ padding: '30% 0 0 0' }} />;
     return (
       <div className={b(null, { blur: this.props.isLightBoxOpen })}>
         <AccountsContainer />
