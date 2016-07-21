32a9decd19dc6052771f087dc26fe0cee2e3b5cc
bokuweb <bokuweb12@gmail.com>
2016-07-21 09:30:25 +0900

    Update notification style

--- src/renderer/src/components/notification.js
+++ src/renderer/src/components/notification.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import NotificationSystem from 'react-notification-system';
 
+const style = {
+  NotificationItem: {
+    DefaultStyle: {
+      padding: '20px 25px',
+      color: '#fff',
+      border: 'none',
+    },
+    success: {
+      background: '#97cd76',
+    },
+    error: {
+      background: '#ed6c63',
+    },
+  },
+};
+
 export default class Notification extends Component {
   constructor(props) {
     super(props);
@@ -20,7 +36,7 @@ export default class Notification extends Component {
 
   render() {
     return (
-      <NotificationSystem ref={this.assignNotificationRef} />
+      <NotificationSystem ref={this.assignNotificationRef} style={style} />
     );
   }
 }
