fa22d9770e53ba8fee867a4141e784715b8fef63
bokuweb <bokuweb12@gmail.com>
2016-08-07 14:10:23 +0900

    Use react-window-component

--- src/renderer/src/components/tweet-window.js
+++ src/renderer/src/components/tweet-window.js
@@ -1,8 +1,8 @@
 /* @flow */
 
 import React, { Component } from 'react';
+import Window from 'react-window-component';
 import B from '../lib/bem';
-import ResizableAndMovable from 'react-resizable-and-movable';
 import Tooltip from 'rc-tooltip';
 import AccountList from './account-list';
 import { isEqual } from 'lodash';
@@ -11,31 +11,28 @@ import Spinner from './spinner';
 import log from '../lib/log';
 import UploadMedia from '../containers/upload-media';
 import TweetWindowHeader from './tweet-window-header';
-import TweetEditor from './tweet-Editor';
+import TweetEditor from './tweet-editor';
 import TweetWindowFooter from './tweet-window-footer';
 
 const b = B.with('tweet-window');
 
 export default class TweetWindow extends Component {
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
     this.state = {
       status: '',
       destroyTooltip: false,
       selectedAccount: props.accounts[0],
       path: null,
-      isDragging: false,
-      isResizing: false,
     };
     this.close = this.close.bind(this);
-    this.onDrag = this.onDrag.bind(this);
     this.onClick = this.onClick.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onAccountSelect = this.onAccountSelect.bind(this);
     this.onSelectFile = this.onSelectFile.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps: Props) {
     const nextId = nextProps.replyTweet.id_str;
     if (nextId && nextId !== this.props.replyTweet.id_str) {
       this.setState({ status: `@${nextProps.replyTweet.user.screen_name} ` });
@@ -51,12 +48,8 @@ export default class TweetWindow extends Component {
     }
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps: Props, nextState: State): bool {
     return !isEqual(this.props, nextProps) || !isEqual(this.state, nextState);
-  }
-
-  onDrag() {
-    this.setState({ destroyTooltip: true, isDragging: true });
   }
 
   onAccountSelect(account) {
@@ -69,7 +62,7 @@ export default class TweetWindow extends Component {
     this.setState({ status: '', destroyTooltip: true });
   }
 
-  onChange(status) {
+  onChange(status: String) {
     this.setState({ status });
   }
 
@@ -144,102 +137,78 @@ export default class TweetWindow extends Component {
     if (remain < 0 || remain === 140) buttonState = 'isDisabled';
     else if (this.props.isPosting) buttonState = 'isLoading';
     return (
-      <div
-        style={
-          this.props.isOpen ? {
-            pointerEvents: this.state.isDragging || this.state.isResizing ? 'auto' : 'none',
-            position: 'absolute',
-            top: '0px',
-            left: '0px',
-            width: '100%',
-            height: '100%',
-            zIndex: 9999,
-          } : {
-            display: 'none',
-          }
-        }
+      <Window
+         isOpen={this.props.isOpen}
+         x={100}
+         y={300}
+         width={380}
+         height={180}
+         minWidth={300}
+         minHeight={150}
+         maxWidth={800}
+         maxHeight={800}
+         style={{
+           backgroundColor: '#fff',
+           pointerEvents: 'auto',
+           padding: this.props.media.length === 0 ? '0 0 4px 0' : '0 0 64px 0',
+         }}
+         className={b()}
       >
-        <ResizableAndMovable
-          x={100}
-          y={300}
-          width={380}
-          height={180}
-          minWidth={300}
-          minHeight={150}
-          maxWidth={800}
-          maxHeight={800}
-          style={{
-            backgroundColor: '#fff',
-            position: 'absolute',
-            top: '0px',
-            left: '0px',
-            pointerEvents: 'auto',
-            padding: this.props.media.length === 0 ? '0 0 4px 0' : '0 0 64px 0',
-          }}
-          bounds="parent"
-          className={b()}
-          onDrag={this.onDrag}
-          onResize={() => this.setState({ isResizing: true })}
-          onResizeStop={() => this.setState({ isResizing: false })}
-          onDragStop={() => this.setState({ isDragging: false })}
-          canUpdateSizeByParent
+        <TweetWindowHeader close={this.close} />
+        <div
+           className={b('body')}
+           onDragOver={() => {
+             if (this.props.media.length >= 4) return null;
+             this.setState({ isDragOver: true })
+        }}
+        onDrop={this.onDropFile.bind(this)}
         >
-          <TweetWindowHeader close={this.close} />
-          <div
-            className={b('body')}
-             onDragOver={() => {
-              if (this.props.media.length >= 4) return null;
-              this.setState({ isDragOver: true })
-            }}
-            onDrop={this.onDropFile.bind(this)}
-          >
-            {this.renderAccount()}
+        {this.renderAccount()}
         <div className={b('textarea-wrapper')}>
           <TweetEditor onChange={this.onChange} />
-              {
-                this.state.isDragOver && !this.props.isMediaUploading
-                  ? <div
-                      style={{
-                        width: '100%',
-                         height: '100%',
-                         background: 'rgba(255, 255, 255, 0.9)',
-                         position: 'absolute',
-                         top: 0,
-                         left: 0,
-                         boxSizing: 'border-box',
-                         flexDirection: 'column',
-                         zIndex: 9999,
-                         display: 'flex',
-                         alignItems: 'center',
-                         justifyContent: 'center',
-                         textAlign: 'center',
-                         color: '#888'
-                         }}
-                         onDragLeave={() => {
-                           this.setState({ isDragOver: false });
-                           console.log('leave');
-                         }}
-                  >
-                  <div style={{fontSize: '48px', pointerEvents: 'none', height: '60px'}}><i className="lnr lnr-picture" /></div><div style={{fontSize: '20px', pointerEvents: 'none'}}>Please drag photo here</div></div>
-                  : null
-
-              }
-              <UploadMedia media={this.props.media} />
-              <TweetWindowFooter
-                remain={remain}
-                onClick={this.onClick}
-                onSelectFile={this.onSelectFile}
-                buttonState={buttonState}
-              />
-            </div>
-          </div>
-      {
-        this.props.isMediaUploading
-          ? <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: '#fff', opacity: '0.7' }}><Spinner style={{ padding: '10% 0 0 80px' }} /></div>
-          : null
-      }
-        </ResizableAndMovable>
-      </div>
+          {
+            this.state.isDragOver && !this.props.isMediaUploading
+              ? <div
+                     style={{
+                       width: '100%',
+                       height: '100%',
+                       background: 'rgba(255, 255, 255, 0.9)',
+                       position: 'absolute',
+                       top: 0,
+                       left: 0,
+                       boxSizing: 'border-box',
+                       flexDirection: 'column',
+                       zIndex: 9999,
+                       display: 'flex',
+                       alignItems: 'center',
+                       justifyContent: 'center',
+                       textAlign: 'center',
+                       color: '#888'
+                     }}
+                     onDragLeave={() => {
+                       this.setState({ isDragOver: false });
+                       console.log('leave');
+                     }}
+              >
+              <div style={{fontSize: '48px', pointerEvents: 'none', height: '60px'}}><i className="lnr lnr-picture" /></div><div style={{fontSize: '20px', pointerEvents: 'none'}}>Please drag photo here</div></div>
+              : null
+            
+          }
+          <UploadMedia media={this.props.media} />
+          <TweetWindowFooter
+             remain={remain}
+             onClick={this.onClick}
+             onSelectFile={this.onSelectFile}
+             buttonState={buttonState}
+             />
+        </div>
+</div>
+{
+  this.props.isMediaUploading
+    ? <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: '#fff', opacity: '0.7' }}><Spinner style={{ padding: '10% 0 0 80px' }} /></div>
+    : null
+}
+  </Window>
     );
   }
 }
