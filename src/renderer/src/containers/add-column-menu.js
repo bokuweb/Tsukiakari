8c36a8a1199d7246bc43c5d96929057f2b60c713
bokuweb <bokuweb12@gmail.com>
2016-09-05 11:16:05 +0900

    Add action

--- src/renderer/src/containers/add-column-menu.js
+++ src/renderer/src/containers/add-column-menu.js
@@ -3,18 +3,20 @@ import { bindActionCreators } from 'redux';
 import AddColumnMenu from '../components/add-column-menu';
 import * as sidemenu from '../actions/sidemenu';
 import * as column from '../actions/column';
+import * as addColumnMenu from '../actions/add-column-menu.js';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: State): Object {
   return {
     accounts: state.accounts.accounts,
     isOpen: state.sidemenu.isAddColumnMenuOpen,
   };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch): Object {
   return {
     close: bindActionCreators(sidemenu.closeAddColumnMenu, dispatch),
     onCreate: bindActionCreators(column.addColumn, dispatch),
+    searchTweets: bindActionCreators(addColumnMenu.searchTweets, dispatch),
   };
 }
 
