0044752259e45c84da1bfac925fb532505dff839
bokuweb <bokuweb12@gmail.com>
2016-09-05 11:15:54 +0900

    Fix re-bulma decls

--- src/decls/re-bulma.js
+++ src/decls/re-bulma.js
@@ -2,4 +2,6 @@ import React from 'react';
 
 declare module 're-bulma' {
   declare var Button: React.Component<*, *, *>;
+  declare var Input: React.Component<*, *, *>;
+  declare var Addons: React.Component<*, *, *>;
 }
